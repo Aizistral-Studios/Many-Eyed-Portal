80deb5eeeb7c77e5ef74e1830104510afd1f6d90
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-11-22 11:39:13 +0100

    Rewrite main mod class

--- src/main/java/com/aizistral/manyeyedportal/ManyEyedPortal.java
+++ src/main/java/com/aizistral/manyeyedportal/ManyEyedPortal.java
@@ -2,32 +2,24 @@ package com.aizistral.manyeyedportal;
 
 import com.aizistral.manyeyedportal.blocks.PortalFrameBlock;
 import com.aizistral.manyeyedportal.handlers.ConfigHandler;
-import com.aizistral.manyeyedportal.handlers.PortalEventHandler;
-import com.aizistral.manyeyedportal.handlers.SuperpositionHandler;
+import com.aizistral.manyeyedportal.handlers.CuriosCompatHandler;
 import com.aizistral.manyeyedportal.items.PortalEyeItem;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EndPortalFrameBlock;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Mod(ManyEyedPortal.MODID)
 public class ManyEyedPortal {
@@ -36,66 +28,52 @@ public class ManyEyedPortal {
 	public static final PortalEyeItem[] PORTAL_EYES = new PortalEyeItem[12];
 	public static final PortalFrameBlock[] FRAME_BLOCKS = new PortalFrameBlock[12];
 	public static final CreativeModeTab CREATIVE_TAB = new CreativeModeTab("manyEyedPortalTab") {
+
 		@Override
-		@OnlyIn(Dist.CLIENT)
 		public ItemStack makeIcon() {
 			return new ItemStack(PORTAL_EYES[0]);
 		}
 	};
 
 	public ManyEyedPortal() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onLoadComplete);
 
-		MinecraftForge.EVENT_BUS.register(this);
-		MinecraftForge.EVENT_BUS.register(new PortalEventHandler());
 
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.getConfig(), "ManyEyedPortal.toml");
 	}
 
-	private void onLoadComplete(FMLLoadCompleteEvent event) {
-		// NO-OP
-	}
-
-	private void setup(FMLCommonSetupEvent event) {
-		// NO-OP
-	}
-
-	private void doClientStuff(FMLClientSetupEvent event) {
-		// NO-OP
-	}
 
 	private void enqueueIMC(InterModEnqueueEvent event) {
-		SuperpositionHandler.registerCurioType("charm", 1, false, null);
+		if (ModList.get().isLoaded("curios")) {
+			CuriosCompatHandler.registerCurioType("charm", 1, false, null);
+		}
 	}
 
-	private void processIMC(InterModProcessEvent event) {
-		// NO-OP
-	}
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MODID)
 	public static class RegistryEvents {
 
-		@SubscribeEvent
-		public static void registerBlocks(RegistryEvent.Register<Block> event) {
-			for (int i = 0; i < FRAME_BLOCKS.length; i++) {
-				event.getRegistry().register(FRAME_BLOCKS[i] = new PortalFrameBlock(i + 1));
-			}
-		}
 
 		@SubscribeEvent
-		public static void registerItems(RegistryEvent.Register<Item> event) {
-			for (int i = 0; i < FRAME_BLOCKS.length; i++) {
-				event.getRegistry().register(new BlockItem(FRAME_BLOCKS[i], new Item.Properties()
-						.rarity(Rarity.EPIC).stacksTo(64).fireResistant().tab(ManyEyedPortal.CREATIVE_TAB))
-						.setRegistryName(MODID, "portal_frame_" + (i + 1)));
-			}
+		public static void registerBlocks(RegisterEvent event) {
 
-			for (int i = 0; i < PORTAL_EYES.length; i++) {
-				event.getRegistry().register(PORTAL_EYES[i] = new PortalEyeItem(i + 1));
+			if (event.getRegistryKey().equals(ForgeRegistries.Keys.BLOCKS)) {
+				for (int i = 0; i < FRAME_BLOCKS.length; i++) {
+					int finalIndex = i;
+					event.register(ForgeRegistries.Keys.BLOCKS, new ResourceLocation(ManyEyedPortal.MODID, "portal_frame_" + i), () -> FRAME_BLOCKS[finalIndex] = new PortalFrameBlock(finalIndex + 1));
+				}
+			}
+			if (event.getRegistryKey().equals(ForgeRegistries.Keys.ITEMS)) {
+				for (int i = 0; i < FRAME_BLOCKS.length; i++) {
+					final int finalIndex = i;
+					event.register(ForgeRegistries.Keys.ITEMS, new ResourceLocation(MODID, "portal_frame_" + (i + 1)), () -> new BlockItem(FRAME_BLOCKS[finalIndex], new Item.Properties()
+							.rarity(Rarity.EPIC).stacksTo(64).fireResistant().tab(ManyEyedPortal.CREATIVE_TAB)));
+				}
+
+				for (int i = 1; i < PORTAL_EYES.length + 1; i++) {
+					final int index = i;
+					event.register(ForgeRegistries.Keys.ITEMS, new ResourceLocation(ManyEyedPortal.MODID, "portal_eye_" + i), () -> PORTAL_EYES[index - 1] = new PortalEyeItem(index));
+				}
 			}
 		}
 
